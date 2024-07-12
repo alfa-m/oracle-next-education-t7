61b8f36dcb416eed5282f8e27a586d94635bcbc1
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-07-12 08:40:19 -0300

    add textos na tela e reinicialização

--- logica-de-programacao-js/curso-2/app.js
+++ logica-de-programacao-js/curso-2/app.js
@@ -1,9 +1,50 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Jogo do número secreto";
+//let titulo = document.querySelector("h1");
+//titulo.innerHTML = "Jogo do número secreto";
 
-let paragrafo = document.querySelector("p");
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+//let paragrafo = document.querySelector("p");
+//paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+function alterarHtml(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
 
 function verificarChute() {
-  console.log("Chute");
+  let chute = document.querySelector("input").value;
+  let tentativaPalavra = tentativa > 1 ? "tentativas" : "tentativa";
+  if (chute == numeroSecreto) {
+    botao.disabled = false;
+    alterarHtml(
+      "#dica_jogo",
+      `Você acertou o número secreto (${numeroSecreto}) em ${tentativa} ${tentativaPalavra}`
+    );
+  } else {
+    if (chute < numeroSecreto) {
+      alterarHtml("#dica_jogo", `O número secreto é maior que ${chute}`);
+    } else {
+      alterarHtml("#dica_jogo", `O número secreto é menor que ${chute}`);
+    }
+  }
+  tentativa++;
 }
+
+function reiniciarJogo() {
+  document.querySelector("input").value = "";
+  alterarHtml("#dica_jogo", "");
+  botao.disabled = true;
+  numeroSecreto = gerarNumeroAleatorio();
+  tentativa = 1;
+}
+
+//let limite = prompt("Insira o número máximo");
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativa = 1;
+let botao = document.querySelector("#reiniciar");
+botao.disabled = true;
+
+alterarHtml("h1", "Jogo do número secreto");
+alterarHtml("#instrucao_jogo", `Escolha um número entre 1 e 10`);
