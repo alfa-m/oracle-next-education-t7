e0a6883117827d94aa027d0c7611fc8f3715ae55
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-07-15 11:32:35 -0300

    add challenge decodificador

---
+++ challenge_decodificador/app.js
@@ -0,0 +1,52 @@
+function criptografa() {
+  let textoEntrada = document.querySelector("#entrada_decodificacao").value;
+  console.log(textoEntrada);
+  let textoSaida = [];
+  for (let i in textoEntrada) {
+    let letra = textoEntrada[i];
+    switch (letra) {
+      case "a":
+        textoSaida.push("ai");
+        break;
+
+      case "e":
+        textoSaida.push("enter");
+        break;
+
+      case "i":
+        textoSaida.push("imes");
+        break;
+
+      case "o":
+        textoSaida.push("ober");
+        break;
+
+      case "u":
+        textoSaida.push("ufat");
+        break;
+
+      default:
+        textoSaida.push(letra);
+        break;
+    }
+  }
+  document.querySelector("#saida_decodificacao").innerHTML =
+    textoSaida.join("");
+}
+
+function descriptografa() {
+  let textoEntrada = document.querySelector("#entrada_decodificacao").value;
+  console.log(textoEntrada);
+  let textoSaida = textoEntrada.replaceAll("ai", "a");
+  textoSaida = textoSaida.replaceAll("enter", "e");
+  textoSaida = textoSaida.replaceAll("imes", "i");
+  textoSaida = textoSaida.replaceAll("ober", "o");
+  textoSaida = textoSaida.replaceAll("ufat", "u");
+  document.querySelector("#saida_decodificacao").innerHTML = textoSaida;
+}
+
+function copiaTexto() {
+  let textoCopiado = document.querySelector("#saida_decodificacao").innerHTML;
+  document.querySelector("#entrada_decodificacao").value = textoCopiado;
+  navigator.clipboard.writeText(textoCopiado);
+}
